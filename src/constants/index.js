eb1b70a8030a0cd4147c8fcb743beb7e7d482dda
fidellim <lim.fidel@gmail.com>
2022-10-06 23:10:17 +0400

    add constants

---
+++ src/constants/index.js
@@ -0,0 +1,74 @@
+export const keys = [
+    {
+        keyCode: 55,
+        value: '7',
+    },
+    {
+        keyCode: 56,
+        value: '8',
+    },
+    {
+        keyCode: 57,
+        value: '9',
+    },
+    {
+        keyCode: 46,
+        value: 'Del',
+    },
+    {
+        keyCode: 52,
+        value: '4',
+    },
+    {
+        keyCode: 53,
+        value: '5',
+    },
+    {
+        keyCode: 54,
+        value: '6',
+    },
+    {
+        keyCode: 107,
+        value: '+',
+    },
+    {
+        keyCode: 49,
+        value: '1',
+    },
+    {
+        keyCode: 50,
+        value: '2',
+    },
+    {
+        keyCode: 51,
+        value: '3',
+    },
+    {
+        keyCode: 189,
+        value: '-',
+    },
+    {
+        keyCode: 190,
+        value: '.',
+    },
+    {
+        keyCode: 48,
+        value: '0',
+    },
+    {
+        keyCode: 191,
+        value: '/',
+    },
+    {
+        keyCode: 106,
+        value: '*',
+    },
+    {
+        keyCode: 27,
+        value: 'Reset',
+    },
+    {
+        keyCode: 187,
+        value: '=',
+    },
+]
