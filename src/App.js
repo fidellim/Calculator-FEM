25322c430c935db5db728d9f71b83d467ee6dbea
fidellim <lim.fidel@gmail.com>
2022-10-06 23:17:38 +0400

    style background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import Calculator from './components/Calculator'
-import logo from './logo.svg'
 
 function App() {
     return (
-        <div className="flex justify-center items-center bg-slate-800 min-w-screen min-h-screen">
+        <div className="flex justify-center items-center bg-mainBackground min-w-screen min-h-screen">
             <main>
                 <Calculator />
             </main>
