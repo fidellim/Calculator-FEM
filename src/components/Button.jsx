760348d61eac044e2fb76d67549f07f690dca908
fidellim <lim.fidel@gmail.com>
2022-10-06 23:16:48 +0400

    add and design button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
 
-const Button = () => {
-    return <div>Button</div>
+const Button = ({ value }) => {
+    const EQUALS = '=',
+        RESET = 'RESET',
+        DELETE = 'DEL',
+        SPACE = ' '
+
+    const isKeyResetOrEqual = (val) =>
+        val.toUpperCase() === RESET || val.toUpperCase() === EQUALS
+
+    const isKeyResetOrDelete = (val) =>
+        val.toUpperCase() === RESET || val.toUpperCase() === DELETE
+
+    const isKeyEquals = (val) => val.toUpperCase() === EQUALS
+
+    const checkValue = (val) => {
+        let res = ''
+
+        if (isKeyResetOrEqual(val)) {
+            res += `col-span-2${SPACE}`
+        }
+
+        if (isKeyResetOrDelete(val)) {
+            res += `bg-delResetBackground${SPACE}text-delResetText`
+        } else if (isKeyEquals(val)) {
+            res += `bg-equalBackground${SPACE}text-white`
+        } else {
+            res += `bg-keyBackground${SPACE}text-keyText`
+        }
+
+        return res
+    }
+
+    return (
+        <div
+            className={`${checkValue(
+                value
+            )} rounded-lg text-center py-2 text-xl`}
+        >
+            {value.toUpperCase()}
+        </div>
+    )
 }
 
 export default Button
