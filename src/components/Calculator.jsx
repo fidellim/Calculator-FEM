5feecfa9cf7845a55d1e07de0cb11d894bab06ce
fidellim <lim.fidel@gmail.com>
2022-10-06 23:17:12 +0400

    add and design calculator component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Button from './Button'
+import { keys } from '../constants'
 
 const Calculator = () => {
     return (
-        <div>
-            <Button />
+        <div className="flex flex-col gap-4 w-[500px]">
+            <div className="flex justify-between">
+                <h1 className="text-default">calc</h1> <div>toggle switch</div>
+            </div>
+            <input
+                type="text"
+                className="text-right bg-screenBackground rounded-lg w-full p-5 text-default"
+            />
+            <div className="grid grid-cols-4 gap-4 bg-keypadBackground p-5 rounded-lg">
+                {keys.map((key) => (
+                    <Button key={key.keyCode} value={key.value} />
+                ))}
+            </div>
         </div>
     )
 }
