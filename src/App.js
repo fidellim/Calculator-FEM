6f552238b01e1244b3c5656cbced4c404cea3934
fidellim <lim.fidel@gmail.com>
2022-10-08 03:12:23 +0400

    add createContext for setting theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Calculator from './components/Calculator'
+import { createContext, useState } from 'react'
+
+const Context = createContext()
 
 function App() {
+    const [theme, setTheme] = useState('')
+
     return (
-        <div className="flex justify-center items-center bg-mainBackground min-w-screen min-h-screen">
-            <main>
-                <Calculator />
-            </main>
-        </div>
+        <>
+            <Context.Provider value={{ setTheme }}>
+                <div
+                    className={`${theme} bg-mainBackground min-w-screen min-h-screen`}
+                >
+                    <main className="flex justify-center items-center min-h-screen p-5">
+                        <Calculator />
+                    </main>
+                </div>
+            </Context.Provider>
+        </>
     )
 }
 
 export default App
+export { Context }
