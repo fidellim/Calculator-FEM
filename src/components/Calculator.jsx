aaeae75da09207a2548511dca673cba0a24da701
fidellim <lim.fidel@gmail.com>
2022-10-08 03:13:08 +0400

    change spacing

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,19 +1,21 @@
-import React, { useEffect } from 'react'
 import Button from './Button'
 import { keys } from '../constants'
 import ToggleSwitch from './ToggleSwitch'
 
 const Calculator = () => {
     return (
-        <div className="flex flex-col gap-4 w-[500px]">
+        <div className="flex flex-col gap-6 w-[475px]">
             <div className="flex justify-between">
-                <h1 className="text-default">calc</h1> <ToggleSwitch />
+                <div>
+                    <h1 className="text-default mt-[20px]">calc</h1>
+                </div>
+                <ToggleSwitch />
             </div>
             <input
                 type="text"
                 className="text-right bg-screenBackground rounded-lg w-full p-5 text-default"
             />
-            <div className="grid grid-cols-4 gap-4 bg-keypadBackground p-5 rounded-lg">
+            <div className="grid grid-cols-4 gap-5 bg-keypadBackground p-5 rounded-lg">
                 {keys.map((key) => (
                     <Button key={key.keyCode} value={key.value} />
                 ))}
