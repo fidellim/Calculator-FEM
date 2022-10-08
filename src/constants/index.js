80ab75d3ccfb9c091ea3064b8a74c6d42c9b5943
fidellim <lim.fidel@gmail.com>
2022-10-09 03:11:04 +0400

    add id key

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,73 +2,91 @@ export const keys = [
     {
         keyCode: 55,
         value: '7',
+        id: 'seven',
     },
     {
         keyCode: 56,
         value: '8',
+        id: 'eight',
     },
     {
         keyCode: 57,
         value: '9',
+        id: 'nine',
     },
     {
         keyCode: 46,
         value: 'Del',
+        id: 'delete',
     },
     {
         keyCode: 52,
         value: '4',
+        id: 'four',
     },
     {
         keyCode: 53,
         value: '5',
+        id: 'five',
     },
     {
         keyCode: 54,
         value: '6',
+        id: 'six',
     },
     {
         keyCode: 107,
         value: '+',
+        id: 'add',
     },
     {
         keyCode: 49,
         value: '1',
+        id: 'one',
     },
     {
         keyCode: 50,
         value: '2',
+        id: 'two',
     },
     {
         keyCode: 51,
         value: '3',
+        id: 'three',
     },
     {
         keyCode: 189,
         value: '-',
+        id: 'subtract',
     },
     {
         keyCode: 190,
         value: '.',
+        id: 'decimal',
     },
     {
         keyCode: 48,
         value: '0',
+        id: 'zero',
     },
     {
         keyCode: 191,
         value: '/',
+        id: 'divide',
     },
     {
         keyCode: 106,
         value: '×',
+        id: 'multiply',
     },
     {
         keyCode: 27,
         value: 'Reset',
+        id: 'clear',
     },
     {
         keyCode: 187,
         value: '=',
+        id: 'equals',
     },
 ]
