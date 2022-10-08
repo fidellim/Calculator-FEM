0adb064f7a9b698c9bdf40728b06128d2058e6c5
fidellim <lim.fidel@gmail.com>
2022-10-09 03:23:24 +0400

    make calculator functional without handling decimals and negative numbers

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import { Context } from '../App'
 import { useContext, useRef } from 'react'
 
-const Button = ({ value }) => {
+const Button = ({ value, id }) => {
     const { setCalculator, calculator } = useContext(Context)
     const buttonRef = useRef()
 
@@ -9,7 +9,8 @@ const Button = ({ value }) => {
         RESET = 'RESET',
         DELETE = 'DEL',
         SPACE = ' ',
-        NEGATIVE = '-'
+        NEGATIVE = '-',
+        OPERATIONS = '+-×/'
 
     const isKeyResetOrEqual = (val) =>
         val.toUpperCase() === RESET || val.toUpperCase() === EQUALS
@@ -62,19 +63,109 @@ const Button = ({ value }) => {
         //    - there should be one decimal only
         //    - if current value is zero, append it with decimal not replace
         // Check "-"
-        if (Number.isInteger(Number(buttonValue)) || buttonValue === NEGATIVE) {
+        if (Number.isInteger(Number(buttonValue))) {
             if (calculator.currNum === '0') {
+                if (calculator.operation && !calculator.prevNum) {
+                    setCalculator((prev) => ({
+                        ...prev,
+                        // prevNum: prev.currNum,
+                        currNum: buttonValue,
+                        calculation: prev.calculation + buttonValue,
+                    }))
+                } else {
+                    let tempVal = ''
+                    if (buttonValue === NEGATIVE) tempVal = '-0'
+                    else tempVal = buttonValue
+
+                    setCalculator((prev) => ({
+                        ...prev,
+                        currNum: tempVal,
+                        calculation: tempVal,
+                    }))
+                }
+            } else {
+                // check if statement has equals
+                if (calculator.calculation.includes(EQUALS)) {
+                    setCalculator((prev) => ({
+                        ...prev,
+                        currNum: buttonValue,
+                        calculation: buttonValue,
+                        operation: undefined,
+                    }))
+                } else {
+                    // check if operation has a value already and prevNum is still empty
+                    // if (calculator.operation && !calculator.prevNum) {
+                    //     setCalculator((prev) => ({
+                    //         ...prev,
+                    //         prevNum: prev.currNum,
+                    //         currNum: buttonValue,
+                    //         calculation: buttonValue,
+                    //     }))
+                    // } else {
+                    // Check if val is -0
+                    let tempVal = ''
+                    if (calculator.currNum === '-0') tempVal = `-${buttonValue}`
+                    else if (OPERATIONS.includes(calculator.currNum))
+                        tempVal = buttonValue
+                    else tempVal = calculator.currNum + buttonValue
+
+                    setCalculator((prev) => ({
+                        ...prev,
+                        currNum: tempVal,
+                        calculation: prev.calculation + buttonValue,
+                        operation: undefined,
+                    }))
+                    // }
+                }
+            }
+            return
+        }
+
+        if (OPERATIONS.includes(buttonValue)) {
+            const lastChar = calculator.calculation.at(-1)
+
+            if (OPERATIONS.includes(lastChar)) {
                 setCalculator((prev) => ({
                     ...prev,
-                    currNum: buttonRef.current.innerHTML,
+                    currNum: buttonValue,
+                    calculation: prev.calculation.slice(0, -1) + buttonValue,
+                }))
+
+                //check if operation is same
+                // if not change operation
+                // console.log(
+                //     eval(
+                //         `${calculator.prevNum} ${calculator.operation} ${calculator.currNum}`
+                //     )
+                // )
+                // check if buttonValue is minus
+                // if minus, allow
+                // else override the operation
+                // if (buttonValue === NEGATIVE) {
+                //     setCalculator((prev) => ({
+                //         ...prev,
+                //         currNum: tempVal,
+                //         calculation: tempVal,
+                //     }))
+                // }
+            } else if (calculator.calculation.includes(EQUALS)) {
+                // if calculation has equals
+                // set calculation to "currNum + buttonValue"
+                // set currNum to "buttonValue"
+                setCalculator((prev) => ({
+                    ...prev,
+                    currNum: buttonValue,
+                    calculation: prev.currNum + buttonValue,
+                    operation: buttonValue,
                 }))
             } else {
                 setCalculator((prev) => ({
                     ...prev,
-                    currNum: prev.currNum + buttonValue,
+                    currNum: buttonValue,
+                    calculation: prev.calculation + buttonValue,
+                    operation: buttonValue,
                 }))
             }
-            return
         }
 
         // Delete last character
@@ -84,11 +175,13 @@ const Button = ({ value }) => {
                 if (prev.currNum.length > 1) {
                     return {
                         ...prev,
+                        calculation: prev.calculation.slice(0, -1),
                         currNum: prev.currNum.slice(0, -1),
                     }
                 }
                 return {
                     ...prev,
+                    calculation: prev.calculation.slice(0, -1) + '0',
                     currNum: '0',
                 }
             })
@@ -100,6 +193,29 @@ const Button = ({ value }) => {
                 prevNum: undefined,
                 currNum: '0',
                 operation: undefined,
+                calculation: undefined,
+            })
+        }
+
+        if (buttonValue === EQUALS) {
+            // check if the statement has equals
+            if (calculator.calculation.includes(EQUALS)) return
+
+            // check if the last character in the calculation is an operation
+            const lastChar = calculator.calculation.at(-1)
+            let statement = calculator.calculation
+            if (OPERATIONS.includes(lastChar) || lastChar === NEGATIVE)
+                statement = calculator.calculation.slice(0, -1)
+
+            // replace multiplication character to x/*
+            statement = statement.replaceAll(/×/g, '*')
+
+            const answer = eval(statement)
+            console.log(answer)
+            setCalculator({
+                currNum: answer,
+                calculation: `${statement}${EQUALS}${answer}`,
+                operation: EQUALS,
             })
         }
     }
@@ -112,7 +228,7 @@ const Button = ({ value }) => {
                 )} keypad z-20 relative rounded sm:rounded-lg text-center hover:translate-y-[.35rem] active:translate-y-[.35rem] h-[48px] w-full flex justify-center items-center`}
                 onClick={handleButton}
             >
-                <h2 ref={buttonRef} className="leading-[1] pt-3 pb-1">
+                <h2 ref={buttonRef} id={id} className="leading-[1] pt-3 pb-1">
                     {value.toUpperCase()}
                 </h2>
             </button>
