3853ba272b36b9e1d569c4c55b45ac7795ac8526
fidellim <lim.fidel@gmail.com>
2022-10-08 17:09:06 +0400

    add useState hook for calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,8 +1,13 @@
 import Button from './Button'
 import { keys } from '../constants'
 import ToggleSwitch from './ToggleSwitch'
+import { Context } from '../App'
+import { useContext } from 'react'
 
 const Calculator = () => {
+    const { calculator } = useContext(Context)
+    // console.log(calculator)
+
     return (
         <div className="flex flex-col gap-6 w-[475px]">
             <div className="flex justify-between">
@@ -11,10 +16,12 @@ const Calculator = () => {
                 </div>
                 <ToggleSwitch />
             </div>
-            <input
+            <div
                 type="text"
                 className="text-right bg-screenBackground rounded-lg w-full p-5 text-default"
-            />
+            >
+                {calculator.currNum}
+            </div>
             <div className="grid grid-cols-4 gap-5 bg-keypadBackground p-5 rounded-lg">
                 {keys.map((key) => (
                     <Button key={key.keyCode} value={key.value} />
