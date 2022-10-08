4f2435ecd93a750fe50a6323e405169e07568026
fidellim <lim.fidel@gmail.com>
2022-10-08 17:12:26 +0400

    add calculator useState hook

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ const Context = createContext()
 
 function App() {
     const [theme, setTheme] = useState('')
+    const [calculator, setCalculator] = useState({
+        prevNum: undefined,
+        currNum: '0',
+        operation: undefined,
+    })
 
     return (
         <>
-            <Context.Provider value={{ setTheme }}>
+            <Context.Provider value={{ setTheme, calculator, setCalculator }}>
                 <div
                     className={`${theme} bg-mainBackground min-w-screen min-h-screen`}
                 >
+                    <p>
+                        {JSON.stringify(calculator, function (k, v) {
+                            return v === undefined ? null : v
+                        })}
+                    </p>
                     <main className="flex justify-center items-center min-h-screen p-5">
                         <Calculator />
                     </main>
